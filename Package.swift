0cf061c4bb839f0f3c9af4a48e53bd92c2b3ce28
김태현 <taehyk528@gmail.com>
2022-04-30 20:34:31 +0900

    [#1] Package description - iOS Platform Version 13 - Swift Languagae Version 5 - Dependencies (SnapKit, Then)

--- Package.swift
+++ Package.swift
@@ -5,24 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "SobokKit",
+    
+    platforms: [
+        .iOS(.v13),
+    ],
+    
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SobokKit",
             targets: ["SobokKit"]),
     ],
+    
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
+        .package(url: "https://github.com/devxoul/Then", from: "3.0.0"),
     ],
+    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SobokKit",
-            dependencies: []),
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "Then", package: "Then"),
+            ]),
         .testTarget(
             name: "SobokKitTests",
             dependencies: ["SobokKit"]),
-    ]
+    ],
+    
+    swiftLanguageVersions: [.v5]
 )
