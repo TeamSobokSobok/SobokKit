61d761c2b5da858283e92879f05f8aa4d3620c70
김태현 <taehyk528@gmail.com>
2022-05-11 23:43:44 +0900

    [#5] BaseViewController

---
+++ Sources/Base/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController.swift
+//  
+//
+//  Created by taehy.k on 2022/05/11.
+//
+
+import UIKit
+
+open class SBBaseViewController: UIViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        style()
+        hierarchy()
+        layout()
+    }
+    
+    /**
+     UI 설정에 관련된 내용을 작성합니다.
+     */
+    open func style() {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    /**
+     계층 구조와 관련된 내용을 작성합니다.
+     */
+    open func hierarchy() {}
+    
+    /**
+     제약 조건 설정과 관련된 내용을 작성합니다.
+     */
+    open func layout() {}
+}
